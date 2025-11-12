bd94ebb3f2c173bdbbdf4e3a5fa6dcf84a096af9
Karin Borm <k.borm@ostfalia.de>
2025-11-12 13:12:24 +0100

    bugfix: insert into table only if it does not yet exist

--- step/adminapprove/lib.php
+++ step/adminapprove/lib.php
@@ -56,7 +56,11 @@ class adminapprove extends libbase {
         $record = new \stdClass();
         $record->processid = $processid;
         $record->status = 0;
-        $DB->insert_record('lifecyclestep_adminapprove', $record);
+        $status = $DB->get_field('lifecyclestep_adminapprove', 'status', array('processid' => $processid));
+        if ($status === false) {
+            // Insert only if it does not exist.
+            $DB->insert_record('lifecyclestep_adminapprove', $record);
+        }
         self::$newcourses++;
         return step_response::waiting();
     }
